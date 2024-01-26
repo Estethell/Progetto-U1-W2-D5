ccb0fe59be11eb5cf7a9441a3501564b48b8843c
Ilaria Rosati <llamavida@gmail.com>
2024-01-26 16:46:20 +0100

    Progetto U1-W2-D5

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -117,13 +117,16 @@ const justTrims = [];
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
 
-// let contatore = 0;
-// while (contatore < 32) {
-//   console.log("numero:", contatore);
-//   contatore++;
-// }
+let contatore = 0;
+let i = 0;
+while (contatore !== 32 && i < numericArray.length) {
+  console.log("numero:", numericArray[i]);
 
-// console.log("Esercizio 9:", contatore);
+  contatore = numericArray[i];
+  i++;
+}
+
+console.log("Esercizio 9:", contatore);
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
